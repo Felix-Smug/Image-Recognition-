3b1043daeaf0c590eaaef30645c32c6a879629db
Felix Nguyen <fnguyen@uoguelph.ca>
2025-04-24 16:14:31 -0400

    update

--- ImageDetectionBots.py
+++ ImageDetectionBots.py
@@ -4,19 +4,26 @@ import time
 import keyboard
 import random
 import win32api, win32con
+import os
 
+screen_x = 960
+screen_y = 540
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0,0)
+    time.sleep(0.01)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
 while 1:
-    
-    try:
 
-        if pyautogui.locateOnScreen('bot1.png') != None:
-            print("I can see it")
-            time.sleep(0.5)
-        else:
-            print("I am unable to see it")
-            time.sleep(0.5)
-            
-    except Exception as e:
-        print(f"Error: {e}")
-        time.sleep(0.5)
+    #if the image on screen is 35% accurate it will click
+    try:
+        if pyautogui.locateOnScreen('ow2Bot.png', grayscale=True, confidence=0.4) != None:
+            print("Image found")
+            click(screen_x, screen_y)
+            time.sleep(1)
+
+    except pyautogui.ImageNotFoundException:
+        print("Image not found")
+        time.sleep(1)
